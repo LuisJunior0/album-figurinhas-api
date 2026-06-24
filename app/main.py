14154ecc1f4c031f3b9bd8349eaa76d66c4da46e
LuisJunior0 <laaos2005@gmail.com>
2026-06-24 15:13:46 -0300

    feat: adicionado model de usuario, relacionamento fk entre figurinhas/usuario e requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.database import engine, Base  
 from app import models 
+
 # Lê todas as classes que herdam de 'Base' e cria as tabelas reais no PostgreSQL usando o 'engine'
 Base.metadata.create_all(bind=engine)
 
--- app/models.py
+++ app/models.py
@@ -1,11 +1,33 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from sqlalchemy.sql import func
 from app.database import Base  
+
+class usuario(Base):
+    __tablename__ = "Usuarios"
+
+    id = Column(Integer, primary_key=True, index=True)
+    
+    nome = Column (String, nullable=False)
+    
+    email = Column(String, nullable=False)
+    
+    senha = Column(String, nullable=False)
+    
+    ativo = Column(Boolean,)
+    
+    admin = Column(Boolean, default=False)
+    
+    def __init__(self, nome, email, senha, ativo=True, admin=False):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.ativo = ativo
+        self.admin = admin
 
 class Figurinha(Base):
     __tablename__ = "figurinhas"
     
-    # ID de autoincremento automatico
+    # ID de autoincremento automatico (No SQLAlchemy chaves primarios são autoincrementas por trás dos panos)
     id = Column(Integer, primary_key=True, index=True)
 
     # Siglas das seleções com limite de 3 caracteres
@@ -20,5 +42,13 @@ class Figurinha(Base):
     # Data e hora de quando a figurinha foi registrada 
     created_at = Column(DateTime, server_default=func.now())
 
-class Usuario(Base):
-    __tablename__ = "Usuario"
+    # ForeignKey para identificação do usuario
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
+
+     
+    def __init__(self, sigla, numero, usuario_id, quantidade=1, created_at=None):
+        self.sigla = sigla
+        self.numero = numero
+        self.quantidade = quantidade
+        self.created_at = created_at
+        self.usuario_id = usuario_id
